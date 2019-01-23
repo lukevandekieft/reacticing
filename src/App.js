31e7dfdc379d0888f29e31d23a185e39e37deb3a
luke <luke@iMac-24.local>
2019-01-23 13:38:53 -0800

    Add functioning state for tests.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,13 @@ import React, { Component } from 'react';
 import './App.scss';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      stateTester: true
+    }
+  }
+
   render() {
     return (
       <div>
