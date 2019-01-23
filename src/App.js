21fadfb1d1d7caeb3f427f8cb6c8cb73f3b32504
luke <luke@iMac-24.local>
2019-01-23 13:15:26 -0800

    Set up basic SCSS React environment.

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+//styles
+import './App.scss';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <p>Hello World!</p>
+      </div>
+    );
+  }
+}
+
+export default App;
